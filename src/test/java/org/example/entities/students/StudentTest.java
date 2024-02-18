aab358ca41e9ce54a1b701db0b58e1bad89ea4dc
Olga Shtoppel <olgashtoppel@gmail.com>
2024-02-18 22:09:37 +0300

    Strategy, Command, Memento patterns (upd)

--- src/test/java/org/example/entities/students/StudentTest.java
+++ src/test/java/org/example/entities/students/StudentTest.java
@@ -7,11 +7,28 @@ class StudentTest {
     @Test
     void StudentUndoSaveTest() {
 
-        Student student = new Student("Vasia");
+        Student st1=new Student("Vasya", 5,4,3);
 
-        student.addGrade(3);
-        student.addGrade(4);
-        student.addGrade(5);
+        System.out.println(st1);
+        st1.addGrade(5);
+        st1.addGrade(3);
+        System.out.println(st1);
+        st1.undo();
+        st1.undo();
+        System.out.println(st1);
+        Save save=st1.getSave();
+        st1.removeGrade(1);
+        st1.removeGrade(1);
+        System.out.println(st1);
+        st1.undo();
+        System.out.println(st1);
+        st1.setName("Petya");
+        System.out.println(st1);
+        st1.undo();
+        System.out.println(st1);
+        save.load();
+        System.out.println(st1);
+
 
 
     }
